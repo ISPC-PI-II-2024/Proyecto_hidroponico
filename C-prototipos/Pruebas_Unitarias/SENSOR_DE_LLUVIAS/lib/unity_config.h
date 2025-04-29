53e14be689fd7ae6b537c2ed4d4ed45dd90a8568
Paola Pantoja <pnapantoja@gmail.com>
2025-04-28 21:05:19 -0300

    Agregar pruebas unitarias del sensor de lluvia

---
+++ C-prototipos/Pruebas_Unitarias/SENSOR_DE_LLUVIAS/lib/unity_config.h
@@ -0,0 +1,9 @@
+#ifndef UNITY_CONFIG_H
+#define UNITY_CONFIG_H
+
+// Configura los detalles según tus necesidades
+#define UNITY_INCLUDE_DOUBLE
+#define UNITY_FIXTURE_T
+#define UNITY_OUTPUT_COLOR
+
+#endif // UNITY_CONFIG_H
